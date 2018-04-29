c4aa5b36d56614592d7e1ac86839a67f3fd75636
Kadeem Palacios <kadeempalacios@gmail.com>
2018-04-28 20:22:11 -0700

    Initial Commit

---
+++ Pillar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pillar
+//
+//  Created by Kadeem Palacios on 4/28/18.
+//  Copyright © 2018 Kadeem Palacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
